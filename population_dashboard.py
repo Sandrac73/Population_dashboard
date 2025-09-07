3cb411fc19c480a9387497640b2c7222782ee005
Sandrac73 <chi871933768@gmail.com>
2025-09-07 14:50:40 -0400

    Update population_dashboard.py

--- population_dashboard.py
+++ population_dashboard.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1L7Gg2_WsAbIgAefeM6AZTqgAjMLK-39p
 """
 
-pip install streamlit
-
 import streamlit as st
 import pandas as pd
 import altair as alt
